6ef344f391de29c0125f1ad7f78ae31f6e3178da
Maddie Weber <maddieweber7@gmail.com>
2023-12-12 09:34:33 -0500

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_nums=[]
+    for i in num_list:
+        if i % 2 == 0:
+            even_nums.append(i)
+    return even_nums
+
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    excited_sentences = []
+    for i in sentence_list:
+        if i != "":
+            excited_sentences.append(f"{i}!")
+    return excited_sentences
